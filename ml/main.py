d90b1a2590f181414a50bf98342eacae469caed6
Daniil Dushenev <Danil.dushenev@gmail.com>
2023-11-10 02:18:22 +0300

    add predict_frame method

--- ml/main.py
+++ ml/main.py
@@ -24,6 +24,35 @@ class WeaponDetectModel():
         self.model_cb = model_cb
         self.processor = processor_vit
 
+    def predict_frame(self, frame, treshold=0.75):
+        results = self.model_people(frame, show_conf=True)
+        for i in range(len(results)):
+            if (results[i].boxes.cls.cpu().size()[0] != 0):
+                x1, y1, x2, y2 = results[i].boxes.xyxy[0]
+                image = results[i].orig_img
+                image_pers = Image.fromarray(image)
+                person_image = image_pers.crop((x1.item(), y1.item(), x2.item(), y2.item()))
+                # plt.imshow(person_image)
+
+                inputs = self.processor(images=person_image, return_tensors="pt").to(device)
+                outputs = self.model_vit(**inputs)
+                pred = self.model_cb.predict(outputs.logits.cpu().detach().numpy())
+
+                if pred > treshold:
+                    print(pred)
+                    label = f'{round(float(pred), 2)} human_with_weapon'
+                    color = (0, 0, 255)
+                else:
+                    label = f'{round(float(1 - pred), 2)} human'
+                    color = (255, 0, 0)
+
+                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), color,
+                              3)  # (0, 255, 0) - цвет прямоугольника (здесь зеленый), 2 - толщина линии
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(frame, label, (int(x1), int(y1) - 10), font, 1.5, color, 2,
+                            cv2.LINE_AA)  # (x1, y1 - 10) - координаты для текста, (0, 255, 0) - цвет текста
+        return frame
+
     def predict_video(self, video_path, output_path="results/out_video.avi", fps=30, treshold=0.75):
 
         cap = cv2.VideoCapture(video_path)
@@ -39,35 +68,7 @@ class WeaponDetectModel():
             # Read a frame from the video
             success, frame = cap.read()
             if success:
-                results = self.model_people(frame, show_conf=True)
-                for i in range(len(results)):
-                    if (results[i].boxes.cls.cpu().size()[0] != 0):
-                        x1, y1, x2, y2 = results[i].boxes.xyxy[0]
-                        image = results[i].orig_img
-                        image_pers = Image.fromarray(image)
-                        person_image = image_pers.crop((x1.item(), y1.item(), x2.item(), y2.item()))
-                        # plt.imshow(person_image)
-
-                        inputs = self.processor(images=person_image, return_tensors="pt").to(device)
-                        outputs = self.model_vit(**inputs)
-                        pred = self.model_cb.predict(outputs.logits.cpu().detach().numpy())
-
-                        if pred > treshold:
-                            label = f'{round(float(pred), 2)} human_with_weapon'
-                            color = (0, 0, 255)
-                        else:
-                            label = f'{round(float(1 - pred), 2)} human'
-                            color = (255, 0, 0)
-
-                        cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), color,
-                                      3)  # (0, 255, 0) - цвет прямоугольника (здесь зеленый), 2 - толщина линии
-                        font = cv2.FONT_HERSHEY_SIMPLEX
-                        cv2.putText(frame, label, (int(x1), int(y1) - 10), font, 1.5, color, 2,
-                                    cv2.LINE_AA)  # (x1, y1 - 10) - координаты для текста, (0, 255, 0) - цвет текста
-
-
-                        # else:
-                        #     results[i].names[0] = 'typical_human'
+                frame = self.predict_frame(frame, treshold=treshold)
                 out.write(frame)
             else:
                 break
@@ -84,4 +85,4 @@ model_cb.load_model("weights/cb_v1")
 
 model = WeaponDetectModel(model_people, model_vit, processor_vit, model_cb)
 
-model.predict_video("test_data/74.Шевченко 286.mp4", "results/output_2.avi")
+
